1d88b241b173011c874e5df4ebcce80cae7d3494
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-08-31 23:52:33 -0500

    Update Menu.java
    Made a loop that asks a user for a number between 1 and 100 and if they input an invalid number they get an error message

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,27 @@
 package com.csc;
 
 // Your code goes here!
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Java Menu!!!");
+
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Enter the number between 1 and 100: ");
+            int n = sc.nextInt();
+
+            if (n > 0 && n < 101) {
+                System.out.println("You entered: " + n);
+                break;
+            }  else {
+                System.out.println("Invalid number! Please try again.");
+                i--;
+            }
+        }
+
+        sc.close();
+    }
+}
